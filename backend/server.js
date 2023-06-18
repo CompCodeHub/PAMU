92c14b1fed1d795e7280dda8b45b3b57fa2def35
Suyash Talekar <Suyashtalekar2000@gmail.com>
2023-06-17 20:23:00 -0400

    ready for production test 1

--- backend/server.js
+++ backend/server.js
@@ -1,4 +1,5 @@
 const express = require("express");
+const path = require("path");
 const mongoose = require("mongoose");
 const cookieParser = require("cookie-parser");
 require("dotenv").config();
@@ -8,14 +9,10 @@ const orderRoutes = require("./routes/orders-routes");
 
 const app = express();
 
-
-
 //middleware
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(cookieParser());
-
-
 
 // routes
 app.use("/api/products", productsRoutes);
@@ -23,12 +20,25 @@ app.use("/api/users", usersRoutes);
 app.use("/api/orders", orderRoutes);
 
 // Route for paypal
-app.get("/api/config/paypal", (req, res) => res.send({id: process.env.PAYPAL_CLIENT_ID}));
+app.get("/api/config/paypal", (req, res) =>
+  res.send({ id: process.env.PAYPAL_CLIENT_ID })
+);
 
+
+
+if (process.env.NODE_ENV === "production") {
+  // set static folder
+  app.use(express.static(path.join(__dirname, "../frontend/build/")));
+
+  // any route that is not defined above will be redirected to index.html
+  app.get("*", (req, res) => {
+    res.sendFile(path.resolve(__dirname, "../frontend",  "build",  "index.html"));
+  });
+} 
 
 //Handles unknown routes
 app.use((req, res, next) => {
-  res.status(404).json({message: "Unknown route"})
+  res.status(404).json({ message: "Unknown route" });
 });
 
 // connect to db
@@ -38,9 +48,9 @@ mongoose
     console.log("connected to database");
 
     // Listening on port
-    app.listen(process.env.PORT, () => {
-      console.log("Server running on port", process.env.PORT);
-    });
+  app.listen(process.env.PORT, () => {
+    console.log("Server running on port", process.env.PORT);
+  });
   })
   .catch((err) => {
     console.log(err);
